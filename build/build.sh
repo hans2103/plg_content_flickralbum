4d50edf2c9c001015579b1f782790e092be2e09b
Hans Kuijpers <hans2103@gmail.com>
2025-06-27 11:39:31 +0200

    update build tools

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-EXTENSION_ZIP_FILENAME="build/plg_content_youtube.zip"
-EXTENSION_ELEMENT="youtube"
+EXTENSION_ZIP_FILENAME="build/plg_content_flickralbum.zip"
+EXTENSION_ELEMENT="flickralbum"
 if [ ! -f "$EXTENSION_ELEMENT.xml" ]; then cd ..; fi
 if [ -f "$EXTENSION_ZIP_FILENAME" ]; then rm $EXTENSION_ZIP_FILENAME; fi
 zip -r $EXTENSION_ZIP_FILENAME language/ layouts/ media/ services/ src/ "$EXTENSION_ELEMENT.php" "$EXTENSION_ELEMENT.xml" script.php --quiet
